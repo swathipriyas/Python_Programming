b6f939479cd946b548204cf41991b05aeeefc102
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-04 12:21:53 +0530

    Add files via upload

---
+++ Beginner_Level/fb.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+trend=['fb','whats up','twitter']
+if 'fb' in trend:
+    print("true")
+else:
+    print("false")
