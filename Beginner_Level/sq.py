7838a7f75d3be9bdbb2b6a45d2ac328b8c9274c0
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-04 11:33:56 +0530

    Add files via upload

---
+++ Beginner_Level/sq.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+#squares=[]
+for x in range(1,11):
+     #squares.append(x**2)
+     #print(squares)
+     squares=[x**2 for x in range(1,11)]
+     print"squares=",squares
