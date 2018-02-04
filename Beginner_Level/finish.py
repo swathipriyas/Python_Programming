42b6db329df9e7391b90c5e2bda5abeb514bf99e
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-04 11:53:25 +0530

    Add files via upload

---
+++ Beginner_Level/finish.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+finishers=['swathi','madhu','hema','devi']
+first_finisher=finishers[0]
+last_finisher=finishers[-1]
+for finisher in finishers:
+    print(finisher)
+first_two=finishers[:3]
+print(first_two)
+
