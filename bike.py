cf006c2cbbc64ef659e51c4ae68b64cb6cc4dccf
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-04 10:19:21 +0530

    Add files via upload

---
+++ bike.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     03/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+bikes=['trek','redline','giant']
+first_bike=bikes[0]
+last_bike=bikes[-1]
+for bike in bikes:
+    print(bike)
+bikes=[]
+bikes.append('trek')
+bikes.append('redline')
+bikes.append('giant')
