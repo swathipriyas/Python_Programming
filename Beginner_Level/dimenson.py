c2b6a7bf49a09d8a7768f3374c2848b21e247529
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-04 12:22:19 +0530

    Add files via upload

---
+++ Beginner_Level/dimenson.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=10
+if a==10:
+ print("equals")
+a=20
+if a>=5:
+    print("greater")
+c=15
+if c<=10:
+    print("less")
+else:
+    print("not less")
