8515e56116509c9e4f58984d5814e73389f891ab
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-20 06:53:00 +0530

    Add files via upload

---
+++ player level/hellontime.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      admin
+#
+# Created:     20/02/2018
+# Copyright:   (c) admin 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+s=(input("enter the no"))
+while(s<=5):
+    print("hello")
