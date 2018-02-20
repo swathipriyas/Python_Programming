b1d1872b69053f943d32cc3b3ce21473717aeb5b
swathipriyas <36121157+swathipriyas@users.noreply.github.com>
2018-02-20 07:04:10 +0530

    Add files via upload

---
+++ player level/print.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      admin
+#
+# Created:     13/02/2018
+# Copyright:   (c) admin 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main
+s=int(input("enter the no"))
+print("hello" * s)
+
